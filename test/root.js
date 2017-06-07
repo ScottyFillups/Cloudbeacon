5608255e39865635f7d77219210af24640f5930b
Philip Scott <pscott@zeptohost.com>
2017-06-06 21:55:46 -0400

    add angular input form

--- test/root.js
+++ test/root.js
@@ -1,7 +1,13 @@
 angular.module('root', ['ngCookies'])
     .controller('index', ['$scope', '$cookies', function($scope, $cookies) {
+        $scope.name;
+        $scope.welcome;
+        $scope.submit = function() {
+            $cookies.put('user', $scope.name);
+             $scope.welcome = 'ur name is ' + $cookies.get('user');
+        };
         if ($cookies.get('user') === undefined) {
-            $scope.welcome = 'ur new!';
+            $scope.welcome = 'ur new! put in a name';
             $cookies.put('user', 'philip');
         } else {
             $scope.welcome = 'ur name is ' + $cookies.get('user');
