195610852d16dd04d8a930dde83d2b9f41e31681
Philip Scott <pscott@zeptohost.com>
2017-06-07 23:55:41 -0400

    add conditional form display, cookie clear, and expiry config

--- test/root.js
+++ test/root.js
@@ -1,15 +1,29 @@
+//put this in a service later? or look up how to do it "properly" in angular
+
+function getExpiry(years) {
+    return new Date(new Date().setFullYear(new Date().getFullYear()+years));
+}
+
 angular.module('root', ['ngCookies'])
     .controller('index', ['$scope', '$cookies', function($scope, $cookies) {
+        var expiry = getExpiry(5);
         $scope.name;
         $scope.welcome;
+        $scope.isuser = true;
         $scope.submit = function() {
-            $cookies.put('user', $scope.name);
-             $scope.welcome = 'ur name is ' + $cookies.get('user');
+            $cookies.put('user', $scope.name, {expires: expiry});
+            $scope.welcome = 'ur name is ' + $cookies.get('user');
+            $scope.isuser = true;
+        };
+        $scope.clear = function() {
+            $cookies.put('user', undefined);
+            $scope.isuser = false;
         };
         if ($cookies.get('user') === undefined) {
             $scope.welcome = 'ur new! put in a name';
-            $cookies.put('user', 'philip');
+            $scope.isuser = false;
         } else {
             $scope.welcome = 'ur name is ' + $cookies.get('user');
+            $scope.isuser = true;
         }
     }]);
