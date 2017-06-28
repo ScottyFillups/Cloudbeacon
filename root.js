a3c11b24958576743fe74d5c43817fed30f31fda
Philip Scott <pscott@zeptohost.com>
2017-06-28 01:07:16 -0400

    Refactor timer

--- root.js
+++ root.js
@@ -1,63 +1,71 @@
+function Timer(ngInterval) {
+    this.tick = function() {
+        return Date.now();
+    };
+}
+
+
 function getExpiry(years) {
     return new Date(new Date().setFullYear(new Date().getFullYear()+years));
 }
 function getTimePeriod(hour) {
     if (hour >= 5 && hour < 12) {
-        return 'Good morning';
+        return "Good morning";
     }
     else if (hour >= 12 && hour < 18) {
-        return 'Good afternoon';
+        return "Good afternoon";
     }
     else if (hour >= 18 && hour < 22) {
-        return 'Good evening';
+        return "Good evening";
     }
     else {
-        return "It's getting late,";
+        return "It's getting late, ";
     }
 }
-angular.module('root', ['ngCookies'])
-    .controller('index', ['$scope', '$cookies', '$interval', function($scope, $cookies, $interval) {
+
+var app = angular.module("root", ["ngCookies", "services"]);
+
+app.controller("index", ["$scope", "$cookies", "$interval", "timer", function($scope, $cookies, $interval, timer) {
         var expiry = getExpiry(5)
         $scope.name;
         $scope.isUser;
-        $scope.time = Date.now();
+        $scope.time = timer.tick();
+        $interval(function() {
+            $scope.time = timer.tick();
+        }, 1000);
         $scope.timestring = getTimePeriod(new Date($scope.time).getHours());
-        function tick() {
-            $scope.time = Date.now();
-        }
-        tick();
-        $interval(tick, 1000);
+        
         $scope.submit = function() {
-            $cookies.put('user', $scope.name, {expires: expiry});
+            $cookies.put("user", $scope.name, {expires: expiry});
             $scope.isUser = true;
         };
         $scope.clear = function() {
-            $cookies.remove('user');
+            $cookies.remove("user");
             $scope.isUser = false;
             angular.forEach($scope.todoList, function(item, index) {
-                $cookies.remove('todo' + index);
+                $cookies.remove("todo" + index);
             });
             $scope.todoList = [];
         };
-        if ($cookies.get('user') !== undefined) {
+        if ($cookies.get("user") !== undefined) {
             $scope.isUser = true;
-            $scope.name = $cookies.get('user');
+            $scope.name = $cookies.get("user");
         } else {
             $scope.isUser = false;
         }
         $scope.todoList = [];
         var i = 0;
         var temp;
-        while ((temp = $cookies.getObject('todo' + i)) !== undefined) {
+        while ((temp = $cookies.getObject("todo" + i)) !== undefined) {
             $scope.todoList.push(temp);
             i++;
         }
         $scope.todoAdd = function() {
             var obj = {todoText: $scope.todoInput, done: false};
             $scope.todoList.push(obj);
-            $scope.todoInput = '';
+            $scope.todoInput = "";
             console.log(i);
-            $cookies.putObject('todo' + i, obj, {expires: expiry});
+            $cookies.putObject("todo" + i, obj, {expires: expiry});
             i++;
         };
         $scope.remove = function() {
@@ -67,8 +75,11 @@ angular.module('root', ['ngCookies'])
                 if (!item.done) {
                     $scope.todoList.push(item);
                 } else {
-                    $cookies.remove('todo' + index);
+                    $cookies.remove("todo" + index);
                 }
             });
         };
-    }])
+    }]);
+
+angular.module("services", [])
+    .service("timer", ["$interval", Timer]);
